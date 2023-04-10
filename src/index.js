348d08b106451328940170cf0c1a5c9c27a603ab
ChristinaFrantsova <mob0964442737@gmail.com>
2023-04-10 14:11:52 +0300

    update settings

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import './css.styles.css';
+import axios from 'axios/dist/node/axios.cjs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Notiflix.Notify.success("Hooray! We found totalHits images.");
+// Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+// Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.";);
